31f5a97a8c6ff8394890b6209a90588005158475
moham <moham@LAPTOP-4EQB4TNE>
2023-08-07 13:17:36 +0300

    Add project files.

---
+++ Task3_2/Employee.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3_2
+{
+    internal class Employee
+    {
+        private String name;
+        private int age;
+        private double salary;
+
+        public Employee(string name, int age, double salary)
+        {
+            this.name = name;
+            this.age = age;
+            this.salary = salary;
+        }
+
+        public String getName()
+        {
+            return name;
+        }
+        public void setName(String name)
+        {
+            this.name = name;
+        }
+        public int getAge()
+        {
+            return age;
+        }
+        public void setAge(int age)
+        {
+            this.age = age;
+        }
+        public double getSalary()
+        {
+            return salary;
+        }
+        public void setSalary(int salary)
+        {
+            this.salary = salary;
+        }
+        
+    public String toString()
+        {
+            return "Employee [name=" + name + ", salary=" + salary + ", age=" + age + "]";
+        }
+    }
+}
---
+++ Task3_2/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Task3_2;
+
+var list = new List<Employee>();
+
+while (true)
+{
+    Console.WriteLine("Enter Employee info (name,age,salary) or -1 to end");
+   
+    string input = Console.ReadLine();
+
+    string[] words = input.Split();
+
+    if (words[0] == "-1")
+    {
+        break;
+    }else if (words.Count() != 3)
+    {
+        Console.WriteLine("Error, Enter three values");
+    }
+    else if (int.Parse(words[1]) < 0 ||  double.Parse(words[2]) <0)
+    {
+        Console.WriteLine("Error, Enter a valid value");
+
+    }
+    else
+    {
+        list.Add(new Employee(words[0],  int.Parse(words[1]), double.Parse(words[2])));
+    }
+}
+
+for (int i = 0; i < list.Count; i++)
+{
+    Console.WriteLine(list[i].toString());
+}
